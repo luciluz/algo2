7bd59ddf596edfad5ff7c02045f5b41146182ae4
luciluz <104660670+luciluz@users.noreply.github.com>
2023-04-03 12:42:08 -0300

    Update Recordatorios.cpp

--- Recordatorios.cpp
+++ Recordatorios.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <list>
 
 using namespace std;
@@ -9,10 +8,10 @@ using uint = unsigned int;
 // Pre: 0 <= mes < 12
 uint dias_en_mes(uint mes) {
     uint dias[] = {
-        // ene, feb, mar, abr, may, jun
-        31, 28, 31, 30, 31, 30,
-        // jul, ago, sep, oct, nov, dic
-        31, 31, 30, 31, 30, 31
+            // ene, feb, mar, abr, may, jun
+            31, 28, 31, 30, 31, 30,
+            // jul, ago, sep, oct, nov, dic
+            31, 31, 30, 31, 30, 31
     };
     return dias[mes - 1];
 }
@@ -22,18 +21,18 @@ uint dias_en_mes(uint mes) {
 
 // Clase Fecha
 class Fecha {
-  public:
+public:
     Fecha(int mes, int dia);
     int mes();
     int dia();
     void incrementar_dia();
 
     // Completar declaraciones funciones
-    #if EJ >= 9 // Para ejercicio 9
+#if EJ >= 9 // Para ejercicio 9
     bool operator==(Fecha o);
-    #endif
+#endif
 
-  private:
+private:
     int mes_;
     int dia_;
 };
@@ -74,15 +73,15 @@ void Fecha::incrementar_dia(){
 
 // Clase Horario
 class Horario {
-    public:
-        Horario(uint hora, uint min);
-        uint hora();
-        uint min();
-        bool operator<(Horario h);
+public:
+    Horario(uint hora, uint min);
+    uint hora();
+    uint min();
+    bool operator<(Horario h);
 
-    private:
-        uint hora_;
-        uint min_;
+private:
+    uint hora_;
+    uint min_;
 };
 
 Horario::Horario(uint hora, uint min) : hora_(hora), min_(min) {}
@@ -104,20 +103,20 @@ bool Horario::operator<(Horario h) {
 // Clase Recordatorio
 
 class Recordatorio {
-    public:
-        Recordatorio(Fecha fecha, Horario horario, string mensaje);
-        Fecha fecha();
-        Horario horario();
-        string mensaje();
+public:
+    Recordatorio(Fecha fecha, Horario horario, string mensaje);
+    Fecha fecha();
+    Horario horario();
+    string mensaje();
 
-    private:
-        Fecha fecha_;
-        Horario horario_;
-        string mensaje_;
+private:
+    Fecha fecha_;
+    Horario horario_;
+    string mensaje_;
 };
 
 Recordatorio::Recordatorio(Fecha fecha, Horario horario, string mensaje)
-    : fecha_(fecha), horario_(horario), mensaje_(mensaje) {}
+        : fecha_(fecha), horario_(horario), mensaje_(mensaje) {}
 
 Fecha Recordatorio::fecha() {return fecha_;}
 Horario Recordatorio::horario() {return horario_;}
@@ -127,26 +126,22 @@ ostream& operator<<(ostream& os, Recordatorio r) {
     os << r.mensaje() << " @ " << r.fecha() << " " << r.horario();
     return os;
 }
-/*
-bool Recordatorio::operator<(Recordatorio r) {
-    return this->horario() < r.horario();
-}*/
 
 // Ejercicio 14
 
 // Clase Agenda
 
 class Agenda {
-    public:
-        Agenda(Fecha fecha_inicial);
-        void agregar_recordatorio(Recordatorio rec);
-        void incrementar_dia();
-        list<Recordatorio> recordatorios_de_hoy();
-        Fecha hoy();
+public:
+    Agenda(Fecha fecha_inicial);
+    void agregar_recordatorio(Recordatorio rec);
+    void incrementar_dia();
+    list<Recordatorio> recordatorios_de_hoy();
+    Fecha hoy();
 
-    private:
-        Fecha fecha_hoy_;
-        list<Recordatorio> recordatorios_;
+private:
+    Fecha fecha_hoy_;
+    list<Recordatorio> recordatorios_;
 };
 
 Agenda::Agenda(Fecha fecha_inicial) : fecha_hoy_(fecha_inicial) {}
@@ -157,51 +152,19 @@ void Agenda::incrementar_dia() { fecha_hoy_.incrementar_dia();}
 
 list<Recordatorio> Agenda::recordatorios_de_hoy() {
     list<Recordatorio> recordatorios_hoy;
-    map<Horario, string> mapa_de_recordatorios;
     for(Recordatorio r: recordatorios_){
         if(r.fecha() == fecha_hoy_) recordatorios_hoy.push_back(r);
     }
+    recordatorios_hoy.sort([](Recordatorio r1, Recordatorio r2) {
+        if (r1.horario() < r2.horario()) return true;
+        else if (r2.horario() < r1.horario()) return false;
+        else return r1.mensaje() < r2.mensaje();
+    });
     return recordatorios_hoy;
 }
 
 Fecha Agenda::hoy() {return fecha_hoy_;}
 
-/*
-list<Recordatorio> ordenarRecordatorios(list<Recordatorio> list_rec){
-    list<Recordatorio> recordatorios_ordenados = list_rec;
-    sort(recordatorios_ordenados.begin(), recordatorios_ordenados.end(), [](Recordatorio a, Recordatorio b){
-        return a.horario() < b.horario();
-    });
-}
-
-
-list<Recordatorio> Agenda::ordenarRecordatorios(list<Recordatorio> list_rec){
-    list<Recordatorio> list_rec_copy = list_rec;
-    list<Recordatorio> rec_ordenados;
-    while(!list_rec_copy.empty()){
-        Recordatorio primer_rec = list_rec_copy.front();
-        for(Recordatorio r: list_rec_copy){
-            if(r.horario() < primer_rec.horario()) primer_rec = r;
-        }
-        rec_ordenados.push_back(primer_rec);
-        list_rec_copy.remove(primer_rec);
-    }
-    return rec_ordenados;
-}
-
-
-list<Recordatorio> Agenda::recordatorios_de_hoy() {
-    list<Recordatorio> recordatorios_hoy;
-    map<Horario, string> mapa_de_recordatorios;
-    for(Recordatorio r: recordatorios_){
-        if(r.fecha() == fecha_hoy_) recordatorios_hoy.push_front(r);
-        mapa_de_recordatorios[r.horario()] = r.mensaje();
-    }
-
-
-    return recordatorios_hoy;
-}
-*/
 
 ostream& operator<<(ostream& os, Agenda a) {
     os << a.hoy() << endl;
