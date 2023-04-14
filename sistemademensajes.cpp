5c45aa6b780cd41ea8f3b1b9b539c259f0dd9b52
luciluz <104660670+luciluz@users.noreply.github.com>
2023-04-14 20:25:09 -0300

    Update sistemademensajes.cpp

--- sistemademensajes.cpp
+++ sistemademensajes.cpp
@@ -4,8 +4,24 @@
 
 SistemaDeMensajes::SistemaDeMensajes(): _conns() {}
 
+SistemaDeMensajes::~SistemaDeMensajes() {
+    for (int i=0; i<4; i++) {
+        if (_conns[i] != nullptr) {
+            delete _conns[i];
+        }
+    }
+}
+
 void SistemaDeMensajes::registrarJugador(int id, string ip) {
-    _conns[id] = new ConexionJugador(ip);
+    if(_conns[id] != nullptr){
+        delete _conns[id];
+        _conns[id] = new ConexionJugador(ip);
+    } else{
+        _conns[id] = new ConexionJugador(ip);
+        }
+}
+void SistemaDeMensajes::desregistrarJugador(int id) {
+    _conns[id] = nullptr;
 }
 
 bool SistemaDeMensajes::registrado(int id) const {
@@ -13,9 +29,13 @@ bool SistemaDeMensajes::registrado(int id) const {
 }
 
 void SistemaDeMensajes::enviarMensaje(int id, string mensaje){
-    _conns[id] ->enviarMensaje(mensaje);
+    _conns[id]->enviarMensaje(mensaje);
 }
 
 string SistemaDeMensajes::ipJugador(int id) const {
     return _conns[id]->ip();
+}
+
+Proxy* SistemaDeMensajes::obtenerProxy(int id) {
+
 }
